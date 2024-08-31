8cab9d97f468558b7541167eb1aa45dd04de8358
ChatBookAI <chatbookai@gmail.com>
2024-08-31 20:14:50 +0800

    CSS

--- api/vendor/ezyang/htmlpurifier/library/HTMLPurifier/Printer/ConfigForm.php
+++ api/vendor/ezyang/htmlpurifier/library/HTMLPurifier/Printer/ConfigForm.php
@@ -71,7 +71,7 @@ class HTMLPurifier_Printer_ConfigForm extends HTMLPurifier_Printer
      */
     public static function getCSS()
     {
-        return file_get_contents(HTMLPURIFIER_PREFIX . '/HTMLPurifier/Printer/ConfigForm.css');
+        return file_get_contents(HTMLPURIFIER_PREFIX . '/HTMLPurifier/Printer/ConfigFormCss.css');
     }
 
     /**
@@ -79,7 +79,7 @@ class HTMLPurifier_Printer_ConfigForm extends HTMLPurifier_Printer
      */
     public static function getJavaScript()
     {
-        return file_get_contents(HTMLPURIFIER_PREFIX . '/HTMLPurifier/Printer/ConfigForm.js');
+        return file_get_contents(HTMLPURIFIER_PREFIX . '/HTMLPurifier/Printer/ConfigFormJs.js');
     }
 
     /**
